889ee8e59b4491b1c06fb6ce1074b4b17c955fbe
Luna <gabrielluna2011002@gmail.com>
2021-03-13 20:26:05 -0300

    Icones e Tela de Login

--- themes/WHP/functions.php
+++ themes/WHP/functions.php
@@ -3,41 +3,36 @@
 function my_login_logo()
 { ?>
     <style type="text/css">
-        body.login {}
+        body.login {
+            background: url(<?php echo get_stylesheet_directory_uri(); ?>/images/hp_wallpaper.png)
+        }
 
-        body.login div#login {}
+        body.login div#login h1 { 
+            background: #000; 
+            padding: 10px;
+            border-radius: 10px 10px 0 0;
+        }
 
-        body.login div#login h1 {}
+        body.login div#login .message {
+            margin-bottom: 20;
+        }
 
-        body.login div#login h1 a {}
+        body.login div#login form#loginform {
+            margin-top: 0;
+            border-radius: 10px 10px;
+        }
+        body.login div#login p#nav {
+            display: none;
+        }
 
-        body.login div#login form#loginform {}
+        body.login div#login form#loginform .button {
+            background: #000;
+            color: #d4af37;
+        }
 
-        body.login div#login form#loginform p {}
-
-        body.login div#login form#loginform p label {}
-
-        body.login div#login form#loginform input {}
-
-        body.login div#login form#loginform input#user_login {}
-
-        body.login div#login form#loginform input#user_pass {}
-
-        body.login div#login form#loginform p.forgetmenot {}
-
-        body.login div#login form#loginform p.forgetmenot input#rememberme {}
-
-        body.login div#login form#loginform p.submit {}
-
-        body.login div#login form#loginform p.submit input#wp-submit {}
-
-        body.login div#login p#nav {}
-
-        body.login div#login p#nav a {}
-
-        body.login div#login p#backtoblog {}
-
-        body.login div#login p#backtoblog a {}
+        body.login div#login p#backtoblog {
+            display: none;
+        }
 
         #login h1 a,
         .login h1 a {
@@ -46,7 +41,6 @@ function my_login_logo()
             width: 284px;
             background-size: 284px 169px;
             background-repeat: no-repeat;
-            padding-bottom: 30px;
         }
     </style>
 <?php }
